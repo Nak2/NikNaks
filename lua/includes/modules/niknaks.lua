26d37ccbace6f1ceeac5089ed178246c231eef9c
Nak <nak@live.dk>
2024-09-01 01:45:50 +0200

    Version 0.51

--- lua/includes/modules/niknaks.lua
+++ lua/includes/modules/niknaks.lua
@@ -3,7 +3,7 @@
 
 AddCSLuaFile()
 -- Make sure to use the newest version of NikNaks.
-local version = 0.50
+local version = 0.51
 if NikNaks and NikNaks.VERSION > version then return end
 
 local file_Find, MsgC, unpack = file.Find, MsgC, unpack
