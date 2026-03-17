ae488311c495bdc6930dc316a01cf0beb16afda8
Nak <nak@live.dk>
2026-03-17 21:51:48 +0100

    Forgot to increment version

--- lua/includes/modules/niknaks.lua
+++ lua/includes/modules/niknaks.lua
@@ -3,7 +3,7 @@
 
 AddCSLuaFile()
 -- Make sure to use the newest version of NikNaks.
-local version = 0.64
+local version = 0.65
 if NikNaks and NikNaks.VERSION > version then return end
 
 local file_Find, MsgC, unpack = file.Find, MsgC, unpack
